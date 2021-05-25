fb2faab3ef247bf6b8cc239f9e94ff51b1eded70
sanchiamaral <sanchiamaral@gmail.com>
2021-05-25 16:00:43 +0700

    add redis config for session

--- application/config/config.php
+++ application/config/config.php
@@ -377,13 +377,21 @@ $config['encryption_key'] = 'rCSiHg4w5NtOYDsFVpK3DxquzePhJywm';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
+$config['sess_driver'] = 'redis';
 $config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_expiration'] = 600;
+$config['sess_save_path'] = 'tcp://127.0.0.1:6379?auth=dummy321';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
-$config['sess_regenerate_destroy'] = FALSE;
+$config['sess_regenerate_destroy'] = TRUE;
+
+// $config['sess_driver'] = 'files';
+// $config['sess_cookie_name'] = 'ci_session';
+// $config['sess_expiration'] = 7200;
+// $config['sess_save_path'] = NULL;
+// $config['sess_match_ip'] = FALSE;
+// $config['sess_time_to_update'] = 300;
+// $config['sess_regenerate_destroy'] = FALSE;
 
 /*
 |--------------------------------------------------------------------------
